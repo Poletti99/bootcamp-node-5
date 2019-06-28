9d365a287fa80c3ca44c5f202f6f39099f1e1027
Victor Poletti <victor.poletti@gastecnologia.com.br>
2019-06-28 15:27:44 -0300

    Teste que verifica se a senha enviada e valida

---
+++ __tests__/integration/session.test.js
@@ -0,0 +1,39 @@
+const request = require('supertest');
+
+const app = require('../../src/app');
+const { User } = require('../../src/app/models');
+const truncate = require('../utils/truncate');
+
+describe('Authentication', () => {
+  beforeEach(async () => {
+    await truncate();
+  });
+
+  it('should be able to authenticate with valid credentials', async () => {
+    const user = await User.create({
+      name: 'Victor Poletti',
+      email: 'victorpoletti2009@hotmail.com',
+      password: '123123'
+    });
+
+    const response = await request(app)
+      .post('/sessions')
+      .send({ email: user.email, password: user.password });
+
+    expect(response.status).toBe(200);
+  });
+
+  it('should not be able to authenticate with invalid credentials', async () => {
+    const user = await User.create({
+      name: 'Victor Poletti',
+      email: 'victorpoletti2009@hotmail.com',
+      password: '123123'
+    });
+
+    const response = await request(app)
+      .post('/sessions')
+      .send({ email: user.email, password: '123456' });
+
+    expect(response.status).toBe(401);
+  });
+});
--- src/routes.js
+++ src/routes.js
@@ -1,15 +1,7 @@
 const routes = require('express').Router();
 
-const { User } = require('./app/models/');
+const SessionController = require('./app/controllers/SessionController');
 
-routes.get('/', async (req, res) => {
-  const user = await User.create({
-    name: 'Victor',
-    email: 'victor@email.com',
-    password_hash: 'adasdaad'
-  });
-
-  return res.json({ user });
-});
+routes.post('/sessions', SessionController.store);
 
 module.exports = routes;
